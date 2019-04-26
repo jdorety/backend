726452867e0801286dfd9801aaf02c2d4c6b0fca
Jonathan Dorety <kyullog@gmail.com>
2019-04-25 21:49:40 -0400

    prettier format

--- data/migrations/20190424224842_createParties.js
+++ data/migrations/20190424224842_createParties.js
@@ -1,21 +1,21 @@
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('parties', tbl => {
+  return knex.schema.createTable("parties", tbl => {
     tbl.increments();
-    tbl.integer('user_id')
+    tbl
+      .integer("user_id")
       .unsigned()
       .references("id")
       .inTable("users")
       .onDelete("CASCADE")
       .onUpdate("CASCADE");
-    tbl.integer('numberGuest').unsigned();
-    tbl.datetime('when');
-    tbl.string('theme');
-    tbl.integer('budget');
-    tbl.integer('spentBudget');
-  }
+    tbl.integer("numberGuest").unsigned();
+    tbl.datetime("when");
+    tbl.string("theme");
+    tbl.integer("budget");
+    tbl.integer("spentBudget");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('parties');
+  return knex.schema.dropTableIfExists("parties");
 };
