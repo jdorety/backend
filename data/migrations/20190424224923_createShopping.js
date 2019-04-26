54999425ff9c9b1a2de4e9ab23970138ffd3c1cb
Jonathan Dorety <kyullog@gmail.com>
2019-04-25 21:50:10 -0400

    complete migrations and succesfully migrate

--- data/migrations/20190424224923_createShopping.js
+++ data/migrations/20190424224923_createShopping.js
@@ -1,8 +1,21 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('shopping', tbl => {
+    tbl.increments();
+    tbl
+      .integer("party_id")
+      .unsigned()
+      .references("id")
+      .inTable("parties")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    tbl.string('item').notNullable();
+    tbl.integer('quantity').defaultTo(1);
+    tbl.boolean('purchased', false);
+    tbl.integer('cost');
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('shopping');
 };
