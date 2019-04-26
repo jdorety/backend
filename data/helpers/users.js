b9b99767bbd9810df0e28b397326eb34d27a1952
Jonathan Dorety <kyullog@gmail.com>
2019-04-26 12:48:13 -0400

    Add getUser helper function

--- data/helpers/users.js
+++ data/helpers/users.js
@@ -1,10 +1,21 @@
 const db = require("../dbConfig.js");
 
 module.exports = {
-  registerUser
+  registerUser,
+  getUser
 };
 
 async function registerUser({ username, password }) {
   await db("users").insert({ username, password });
-  return db("users").where({ username }).first();
+  return db("users")
+    .select("username", "id")
+    .where({ username })
+    .first();
+}
+
+function getUser({ id }) {
+  return db("users")
+    .select("username", "password")
+    .where({ id })
+    .first();
 }
--- data/helpers/users.test.js
+++ data/helpers/users.test.js
@@ -1,19 +1,23 @@
-const users = require('./users.js');
-const db = require('../dbConfig.js');
+const users = require("./users.js");
+const db = require("../dbConfig.js");
 
 describe("Database Helpers", () => {
-
   beforeEach(() => {
-    return db('users').truncate();
-  })
+    return db("users").truncate();
+  });
 
-  describe('users table helpers', () => {
-    it('adds user to users table', async () => {
-      const testUser = {username: "test", password: "test"};
+  describe("users table helpers", () => {
+    it("adds user to users table", async () => {
+      const testUser = { username: "test", password: "test" };
       const success = await users.registerUser(testUser);
-      expect(success).toEqual({...testUser, id: 1})
-    })
+      expect(success).toEqual({ username: "test", id: 1 });
+    });
 
-    
-  })
-})
+    it('retrieves user from user object with id', async () => {
+      const user = {username: "testname", password: "testpass"}
+      await db('users').insert(user);
+      const gotUser = await users.getUser({id: 1});
+      expect(gotUser).toEqual(user)
+    });
+  });
+});
