c9dcacf460a21fab1cb3f2db0080917b83c1842e
Jonathan Dorety <kyullog@gmail.com>
2019-04-27 19:50:18 -0400

    link all helpers to one export file

---
+++ data/helpers/dbHelpers.js
@@ -0,0 +1,7 @@
+module.exports = {
+entertainment: require('./entertainment/entertainment.js'),
+parties: require('./parties/parties.js'),
+shopping: require('./shopping/shopping.js'),
+todos: require('./todos/todos.js'),
+users: require('./users/users.js')
+}
