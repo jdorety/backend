ecb924aacce66d4e69c01ed074973c5899eaa0f7
Jonathan Dorety <kyullog@gmail.com>
2019-04-28 17:47:27 -0400

    Complete PUT and DELETE endpoints

--- api/routers/todosRouter.js
+++ api/routers/todosRouter.js
@@ -1,16 +1,21 @@
 const router = require("express").Router();
 
-const { todos } = require("../../data/helpers/dbHelpers.js");
+const { todos, parties } = require("../../data/helpers/dbHelpers.js");
 
 router.post("/", async (req, res) => {
   const newItem = req.body;
   if (newItem.party_id && newItem.item) {
     try {
-      const success = await todos.add(newItem);
-      if (success) {
-        res.status(201).json({ id: success[0] });
+      const verifyParty = await parties.get(newItem.party_id);
+      if (verifyParty) {
+        const success = await todos.add(newItem);
+        if (success) {
+          res.status(201).json({ id: success[0] });
+        } else {
+          res.status(400).json({ err: "Couldn't add item" });
+        }
       } else {
-        res.status(400).json({ err: "Couldn't add item" });
+        res.status(404).json({ err: "Party does not exist" });
       }
     } catch (err) {
       console.log(err);
@@ -20,6 +25,41 @@ router.post("/", async (req, res) => {
     res
       .status(400)
       .json({ err: "Please include a party_id and item in request body" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const editItem = req.body;
+  const { id } = req.params;
+  try {
+    if (editItem.item || editItem.completed) {
+      const edited = await todos.update(id, editItem);
+      if (edited) {
+        res.status(201).json(edited);
+      } else {
+        res.status(400).json({ err: "Couldn't edit specified entry" });
+      }
+    } else {
+      res.status(400).json({ err: "Please include valid entry" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: "Error!" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleted = await todos.delete(id);
+    if (deleted) {
+      res.status(204).json({ message: "Entry deleted" });
+    } else {
+      res.status(400).json({ err: "Couldn't delete message" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: "Error!" });
   }
 });
 
