c366b9692d824cffbfde03a07efee35e181649ac
Jonathan Dorety <kyullog@gmail.com>
2019-04-27 21:25:38 -0400

    create and link userRouter

---
+++ api/routers/userRouter.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const {users, parties} = require('../../data/helpers/dbHelpers.js');
+
+const genericError = {err: "There was a problem processing your request"};
+
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+  const user = await users.getUser(id);
+  if (user) {
+  res.status(200).json(user);
+  } else {
+    res.status(404).json({err: "User not found"})
+  }
+  } catch {
+    res.status(500).json(genericError);
+  }
+})
+
+router.get('/:id/parties', async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const partyList = await parties.getList(userId);
+    if(partyList) {
+      res.status(200).json(partyList);
+    } else {
+      res.status(404).json({err: "No parties associated with user"})
+    }
+  } catch {
+    res.status(500).json(genericError);
+  }
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
+const userRouter = require('./routers/userRouter.js');
+
 const server = express();
 
 server.use(cors(), helmet(), express.json());
+
+server.use('/api/users', userRouter)
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to build week!" });
