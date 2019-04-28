95f585a884beb430bd99cbfa4cd7faa236a09f6d
Jonathan Dorety <kyullog@gmail.com>
2019-04-28 11:29:06 -0400

    Link todosRouter to server

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const helmet = require("helmet");
 
 const userRouter = require("./routers/userRouter.js");
 const partiesRouter = require("./routers/partiesRouter.js");
+const todosRouter = require("./routers/todosRouter.js");
 
 const server = express();
 
@@ -11,6 +12,7 @@ server.use(cors(), helmet(), express.json());
 
 server.use("/api/user", userRouter);
 server.use("/api/party", partiesRouter);
+server.use("/api/todos", todosRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to build week!" });
