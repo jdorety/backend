fb83dae19115b1daf7ff5d38baf28894d92c6cdc
Jonathan Dorety <kyullog@gmail.com>
2019-04-30 00:28:23 -0400

    POST party request responds w/ id

--- api/routers/partiesRouter.js
+++ api/routers/partiesRouter.js
@@ -93,7 +93,7 @@ router.post("/", async (req, res) => {
       if (verifyUser) {
         //adds party if valid user
         const newParty = await parties.add(party);
-        res.status(201).json(newParty);
+        res.status(201).json({ id: [newParty] });
       } else {
         res.status(404).json({ err: "Specified user does not exist" });
       }
