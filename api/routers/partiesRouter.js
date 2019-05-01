1da1e800b286bf8335bc9bdc3da3529954c7f65c
Jonathan Dorety <kyullog@gmail.com>
2019-04-30 23:51:20 -0400

    test authentication on deploy

--- api/routers/partiesRouter.js
+++ api/routers/partiesRouter.js
@@ -6,6 +6,7 @@ const {
   shopping,
   users
 } = require("../../data/helpers/dbHelpers.js");
+const restrict = require("../../authentication/auth-middleware");
 
 const genericError = { err: "There was a problem processing your request" };
 //return object with all party information
@@ -50,7 +51,7 @@ router.get("/:id/todos", async (req, res) => {
   }
 });
 //return array of entertainment records associated with party id passed in URL
-router.get("/:id/entertainment", async (req, res) => {
+router.get("/:id/entertainment", restrict, async (req, res) => {
   const { id } = req.params;
   try {
     const list = await entertainment.getList(id);
