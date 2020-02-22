80e888c4476b84725f97696a1a7d76f5fd269576
Jonathan Dorety <kyullog@gmail.com>
2020-02-22 10:10:30 -0800

    Comment out auth middleware

--- api/routers/partiesRouter.js
+++ api/routers/partiesRouter.js
@@ -7,7 +7,7 @@ const {
   users,
   moodBoard
 } = require("../../data/helpers/dbHelpers.js");
-const restrict = require("../../middleware/auth-middleware");
+// const restrict = require("../../middleware/auth-middleware");
 
 const genericError = { err: "There was a problem processing your request" };
 //return object with all party information
