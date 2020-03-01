e68a1ad29bb81bdd5288042cee51b8d9b631b831
Jonathan Dorety <kyullog@gmail.com>
2020-03-01 14:29:20 -0800

    Add variety to list items

--- data/seeds/04-shopping.js
+++ data/seeds/04-shopping.js
@@ -5,13 +5,13 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex("shopping").insert([
-        { party_id: 1, item: "lawn darts", quantity: 5 },
+        { party_id: 1, item: "lawn darts", quantity: 5, purchased: true },
         { party_id: 2, item: "cd players", quantity: 4 },
         { party_id: 3, item: "ping-pong table", quantity: 3 },
         { party_id: 4, item: "plastic guitar", quantity: 2 },
-        { party_id: 5, item: "Xbox", quantity: 1 },
+        { party_id: 5, item: "Xbox", quantity: 1, purchased: true },
         { party_id: 1, item: "plastic cups", quantity: 2 },
-        { party_id: 2, item: "drinks", quantity: 3 },
+        { party_id: 2, item: "drinks", quantity: 3, purchased: true, cost: 2 },
         { party_id: 3, item: "coasters", quantity: 4 },
         { party_id: 4, item: "slip n' slide", quantity: 5 }
       ]);
