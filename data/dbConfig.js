62bfe161564930b01454384055c8adc17571be6b
Jonathan Dorety <kyullog@gmail.com>
2020-03-05 14:15:16 -0800

    Fix env variable in db config

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,5 +1,5 @@
 const knex = require('knex');
-const environment = process.env.DB_ENV || "development";
+const environment = process.env.NODE_ENV || "development";
 const config = require('../knexfile.js');
 
 const knexConfig = (config[environment])
