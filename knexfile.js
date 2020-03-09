00a5efcfa7d66f5ec69555cc9d92bccf0045faff
Jonathan Dorety <jonathan.dorety@gmail.com>
2020-03-08 17:13:51 -0700

    Update for current dev env

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,16 @@
 // Update with your config settings.
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
   development: {
     client: "pg",
-    connection: "postgres://localhost/partyplanner",
+    connection: {
+      host: '127.0.0.1',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: 'partyplanner',
+      charset: 'utf8'
+    },
     useNullAsDefault: true,
     migrations: {
       directory: "./data/migrations"
